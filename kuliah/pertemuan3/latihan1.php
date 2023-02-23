e5e4d50d256b052baf36c7ba05a9f127f17c0c9b
rayhanrezki <rayhanalfarezki@gmail.com>
2023-02-23 14:46:34 +0700

    tugas

---
+++ kuliah/pertemuan3/latihan1.php
@@ -0,0 +1,18 @@
+<?php  
+echo "Mulai <br>" ; 
+while  (false) {
+    echo "Hello World!!<br>" ;
+}
+echo "Selesai <br>" ; 
+?>
+<br>
+<br>
+<?php  
+echo "Mulai <br>" ;
+$nilai_awal = 1; 
+while  ($nilai_awal <= 5 ) {
+    echo "Hello World!!<br>" ;
+    $nilai_awal += 1;
+}
+echo "Selesai <br>" ; 
+?>
